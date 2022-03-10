a5fc9aff53cd989c741cd91176b91dee038cfb2e
Robert Guske <rguske@vmware.com>
2022-03-10 15:47:05 +0100

    updated versions for photon,docker-compose and harbor
    Closes: #1
    Signed-off-by: Robert Guske <rguske@vmware.com>

--- scripts/photon-settings.sh
+++ scripts/photon-settings.sh
@@ -25,13 +25,12 @@ echo '> Creating directory for setup scripts'
 mkdir -p /root/setup
 
 echo ' > Downloading Harbor...'
-HARBOR_VERSION=2.3.0
+HARBOR_VERSION=2.4.1
 curl -L https://github.com/goharbor/harbor/releases/download/v${HARBOR_VERSION}/harbor-offline-installer-v${HARBOR_VERSION}.tgz -o harbor-offline-installer-v${HARBOR_VERSION}.tgz
 tar xvzf harbor-offline-installer*.tgz
 rm -f harbor-offline-installer-v${HARBOR_VERSION}.tgz
 
 echo '> Downloading docker-compose...'
-DOCKER_COMPOSE_VERSION=1.29.2
-curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+DOCKER_COMPOSE_VERSION=2.3.3
+curl -L "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
